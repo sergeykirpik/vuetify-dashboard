01783b6506bceea8eb7bbbd7cbc52fe9436b6beb
Sergey Kirpik <sergeykirpik@gmail.com>
2021-05-13 11:12:22 +0300

    Implement Login form with Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-row justify="center" align="start">
+      <v-card width="400px" class="mt-10">
+        <v-card-title class="pb-3">
+          <h1>Login</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+            <v-text-field
+              :type="passwordFieldType"
+              label="Password"
+              prepend-icon="mdi-lock"
+              :append-icon="passwordVisibilityIcon"
+              @click:append="togglePasswordVisibility"
+            />
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-btn color="success">Register</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn color="info">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-row>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {},
+
+  data: () => ({
+    showPassword: false,
+  }),
+  methods: {
+    togglePasswordVisibility() {
+      this.showPassword = !this.showPassword;
+    },
+  },
+  computed: {
+    passwordFieldType() {
+      return this.showPassword ? "text" : "password";
+    },
+    passwordVisibilityIcon() {
+      return this.showPassword ? "mdi-eye" : "mdi-eye-off";
+    },
+  },
+};
+</script>
